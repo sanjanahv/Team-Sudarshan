d895204ff7af8d16a090b13a6874d089ac1e2426
Inika15 <inikarao15@gmail.com>
2025-12-04 23:51:59 +0530

    Update Risk_engine.py

--- Inika/Risk_engine.py
+++ Inika/Risk_engine.py
@@ -6,6 +6,14 @@ dealers=pd.read_csv('Gov_dealers.csv')        #Government data of dealers
 
 df=claims.merge(farmers, how='left', on='farmer_id')
 df=df.merge(dealers, how='left', on='dealer_id')
-velocity = df.groupby('dealer_id').agg({'claim_id':'count'}).reset_index()  
+velocity = df.groupby(['Dealer_ID','Date']).size() 
+
+score=0 #Initialise risk score to zero
+def identity_risk(farmer_name, dealer_name):
+    if pd.isna(farmer_name): #if farmer name not present in govt records
+        score+=60
+    if pd.isna(dealer_name): #if dealer name not present in govt records
+        score+=40
+    return score
 
 
