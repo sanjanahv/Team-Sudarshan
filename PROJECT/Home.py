253bed70f9d593813573685faf41acffa1a5ea14
Inika15 <inikarao15@gmail.com>
2025-12-05 08:51:26 +0530

    Add files via upload

---
+++ PROJECT/Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="AgriGuard", layout="wide")
+
+st.title("🌾 AgriGuard – Farmer Verification System")
+st.write("This is the main page.")
+
+st.sidebar.title("Navigation")
+st.sidebar.write("Use the sidebar to switch between pages.")
